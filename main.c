6e9ad4a5eb4014c0571ff1d2f47fb2bb19d54f0b
yasser <ymeftahi@xperlean.com>
2024-03-12 12:24:45 +0000

    crypt

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+void cryptageParDecalage(char *chaine, int decalage) {
+    for (int i = 0; chaine[i] != '\0'; ++i) {
+        
+        if (chaine[i] >= 'A' && chaine[i] <= 'Z') {
+            chaine[i] = (chaine[i] - 'A' + decalage) % 26 + 'A';
+        }
+        
+        else if (chaine[i] >= 'a' && chaine[i] <= 'z') {
+            chaine[i] = (chaine[i] - 'a' + decalage) % 26 + 'a';
+        }
+        
+    }
+}
+
+int main() {
+    char chaine[100];
+    int decalage;
+
+    
+    printf("Entrez la chaîne à crypter : ");
+    fgets(chaine, sizeof(chaine), stdin);
+
+    
+    printf("Entrez le décalage pour le cryptage : ");
+    scanf("%d", &decalage);
+
+    
+    cryptageParDecalage(chaine, decalage);
+
+    
+    printf("Chaîne cryptée : %s", chaine);
+
     return 0;
 }
